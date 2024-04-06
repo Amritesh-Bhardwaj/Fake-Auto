29c17ef7315812b79558745a633edb0307070a79
Amritesh Bhardwaj <bhamritesh@gmail.com>
2024-04-06 21:07:05 +0530

    Navi

---
+++ my-app/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>
+                Homescreen
+            </Text>
+        </View>
+
+    )
+
+
+
+}
+export default HomeScreen;
